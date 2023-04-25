843a55d8685f0b2e6395b520c6e3594c9f14e4a0
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2023-04-25 08:31:22 -0400

    Add a listener and function to add .selected

--- js/mainJS.js
+++ js/mainJS.js
@@ -9,5 +9,11 @@ function hoverOut () {
   this.classList.remove('hover');
 }
 
+function buttonClick () {
+  if (this.textContent === "Clear canvas") return;
+  this.classList.toggle('selected');
+}
+
 const buttons = document.querySelectorAll('.frame1 button');
 buttons.forEach( (button) => button.addEventListener('mouseover', hoverIn) );
+buttons.forEach( (button) => button.addEventListener('click', buttonClick) );
