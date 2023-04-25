cfde0497e662026bedb980aa70fe0bb3035f131a
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2023-04-25 08:55:22 -0400

    Create listener + func. to show value from slider

--- js/mainJS.js
+++ js/mainJS.js
@@ -14,6 +14,14 @@ function buttonClick () {
   this.classList.toggle('selected');
 }
 
+function inputChangeHandler () {
+  const selection = document.querySelector('.sliderContainer .text');
+  selection.textContent = `${this.value} x ${this.value}`;
+}
+
 const buttons = document.querySelectorAll('.frame1 button');
 buttons.forEach( (button) => button.addEventListener('mouseover', hoverIn) );
 buttons.forEach( (button) => button.addEventListener('click', buttonClick) );
+
+const rangeInput = document.querySelector('input[type="range"]');
+rangeInput.addEventListener('input',inputChangeHandler);
